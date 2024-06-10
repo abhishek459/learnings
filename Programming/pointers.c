5ad97b28f6b6543f2d060017773d9475d22d1ba3
Abhishek Thulasi <abhishek45nine@gmail.com>
2024-06-10 20:26:37 +0530

    Code correction in Pointers

--- Programming/pointers.c
+++ Programming/pointers.c
@@ -7,19 +7,19 @@ int main() {
     int *p;
     p = &a;
     
-    printf("%d", a); // Prints 10
-    printf("%d %d", *p, &a); // Prints 10 and Address of a
+    printf("\n%d", a); // Prints 10
+    printf("\n%d\t%d", *p, &a); // Prints 10 and Address of a
     
     *p = 20; // Modifies the data at address of variable a to 20
-    printf("%d", a); // Prints 20
+    printf("\n%d", a); // Prints 20
 
     // Array Pointer Declaration with Array in Stack
-    int A[5] = {2,4,6,8,10};
+    int Array[5] = {2,4,6,8,10};
     int *p2;
-    p2 = A; // This can also be declared as p2 = &A[0]
+    p2 = Array; // This can also be declared as p2 = &Array[0]
 
     for (int i = 0; i < 5; i++) {
-        printf("\n%d", *p2[i]);
+        printf("\n%d", p2[i]);
     }
 
     // Array Pointer Declaration with Array in Heap
@@ -30,6 +30,7 @@ int main() {
     for (int i = 0; i < 5; i++) {
         printf("\n%d", p3[i]);
     }
+    free(p3);
 
     return 0;
 }
