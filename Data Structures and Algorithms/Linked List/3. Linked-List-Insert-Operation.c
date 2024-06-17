333f4f5d3e3395b96a2419ac579674c9db94256d
Abhishek Thulasi <abhishek45nine@gmail.com>
2024-06-17 22:10:50 +0530

    Insert operation

--- Data Structures and Algorithms/Linked List/3. Linked-List-Insert-Operation.c
+++ Data Structures and Algorithms/Linked List/3. Linked-List-Insert-Operation.c
@@ -60,6 +60,51 @@ void insert(struct Node *p, int index, int value) {
     }
 }
 
+struct Node *last;
+
+void insertLast(int value) {
+    if (last == NULL) {
+        struct Node *p = first;
+        while (p->next != NULL) {
+            p=p->next;
+        }
+        last = p;
+    }
+    struct Node *t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = value;
+    t->next = NULL;
+    
+    if (first == NULL) {
+        first = last = t;
+    } else {
+        last->next = t;
+        last=t;
+    }
+}
+
+void sortedInsert(struct Node *p, int x) {
+    struct Node *q, *t = NULL;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    t->data=x;
+    t->next=NULL;
+
+    if (first == NULL) { // If the linked list is empty or doesn't exist
+        first=t;
+    } else {
+        while (p && p->data < x) { // While new element is larger than current element, keep iterating
+            q=p;
+            p=p->next;
+        }
+        if (p==first) { // If new element is smaller than the first element
+            t->next=first;
+            first=t;
+        } else {
+            t->next=q->next;
+            q->next=t;
+        }
+    }
+}
+
 void main() {
     int A[] = {3,5,7,9,11};
     
@@ -77,5 +122,21 @@ void main() {
 
     insert(first, 7, 10);
     printf("\nInsert 10 at index 7:\n");
+    display(first);
+
+    insertLast(11);
+    printf("\nInsert 11 at index 8:\n");
+    display(first);
+
+    insertLast(12);
+    printf("\nInsert 12 at index 9:\n");
+    display(first);
+
+    sortedInsert(first, 8);
+    printf("\nInserting 8:\n");
+    display(first);
+
+    sortedInsert(first, 0);
+    printf("\nInserting 0:\n");
     display(first);
 }
