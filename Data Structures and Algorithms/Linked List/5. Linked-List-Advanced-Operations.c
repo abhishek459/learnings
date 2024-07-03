f2b5b8af9901eb046333a252e7aa9665e23bd050
Abhishek Thulasi <abhishek45nine@gmail.com>
2024-07-03 21:59:28 +0530

    Linked List concatenation and reverse

--- Data Structures and Algorithms/Linked List/5. Linked-List-Advanced-Operations.c
+++ Data Structures and Algorithms/Linked List/5. Linked-List-Advanced-Operations.c
@@ -26,6 +26,26 @@ void create(int A[], int n) {
     }
 }
 
+// Create linkedlist and return its pointer
+struct Node * createDynamically(int A[], int n) {
+    int i = 0;
+    struct Node *p,*t,*last;
+    p=(struct Node *)malloc(sizeof(struct Node));
+    p->data=A[0]; // You can access a struct pointer's struct data using (*p).data as well!
+    p->next=NULL; // -> is used due to its easy syntax.
+    last=p;
+    
+    for (i=1;i<n;i++) {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+
+    return p;
+}
+
 // Display linked list elements iteratively
 void display(struct Node *p) {
     while(p!=NULL) {
@@ -74,12 +94,59 @@ void removeDuplicates(struct Node *p) {
     }
 }
 
+void reverse(struct Node *p) {
+    struct Node *q=NULL, *r=NULL;
+    while (p!=NULL) {
+        r=q; // Slide the pointers
+        q=p; // Slide the pointers
+        p=p->next; // Slide the pointers
+        q->next=r; // Assign the mid pointer to its previous pointer
+    } // Repeat the above process until the end of linked list
+    first=q;
+}
+
+void reverseRecursively(struct Node *q, struct Node *p) {
+    if (p!=NULL) {
+        reverseRecursively(p,p->next);
+        p->next=q;
+    } else {
+        first = q;
+    }
+}
+
+struct Node * concatenate(struct Node *p, struct Node *q) {
+    struct Node *third = p;
+    while (p->next != NULL) {
+        p=p->next;
+    }
+    p->next = q;
+    printf("\nConcatenated linked lists\n");
+    display(third);
+}
+
 int main() {
-    int A[] = {10,10,30,30,50};
+    int A[] = {10,20,30,40,50};
     create(A,5);
     display(first);
     isSorted(first);
+    
     removeDuplicates(first);
+
     printf("\nDuplicates removed:\n");
     display(first);
+
+    printf("\nReversing a linked list\n");
+    reverse(first);
+    display(first);
+
+    printf("\nReversing a linked list recursively\n");
+    reverseRecursively(NULL, first);
+    display(first);
+    
+    int B[] = {1,2,3,4,5};
+    struct Node *second = createDynamically(B, 5);
+    printf("\nSecond Linked List\n");
+    display(second);
+    
+    concatenate(first, second);
 }
